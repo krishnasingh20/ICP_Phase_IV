b483d3b5ffa8eb2e88f6cbc2e58a234af3b5eb00
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-13 14:31:07 +0530

    Modify stock profit calculation with user input
    Refactor the solution to include user input for stock prices.

--- Week1/Best_Time_to_Buy_and_Sell_Stock.java
+++ Week1/Best_Time_to_Buy_and_Sell_Stock.java
@@ -1,5 +1,15 @@
-class Solution {
-    public int maxProfit(int[] prices) {
+import java.util.*;
+public class Best_Time_to_Buy_and_Sell_Stock {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] prices = new int[n];
+        for(int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+        System.out.println(maxProfit(prices));
+    }
+    public static int maxProfit(int[] prices) {
         int profit = 0;
         int curr = prices[0];
         for(int i = 1; i < prices.length; i++) {
