c428908fab867d3af58452b786aee5989c55e183
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-16 11:24:26 +0530

    Refactor findMedianSortedArrays to merge and sort arrays
    Refactored the findMedianSortedArrays method to merge and sort two arrays instead of using binary search.

--- Week1/Median _of_Two_Sorted_Arrays.java
+++ Week1/Median _of_Two_Sorted_Arrays.java
@@ -14,45 +14,24 @@ public class Main {
     }
     System.out.println(findMedianSortedArrays(a, b));
   }
-  public static double findMedianSortedArrays(int[] a, int[] b) {
-      if(a.length > b.length) {
-          int[] temp = a;
-          a = b;
-          b = temp;
-      }
-      int n = a.length;
-      int m = b.length;
-      int t = n+m;
-      int h = (t+1)/2;
-      int low = 0;
-      int high = n;
-      while(low <= high) {
-          int al = (low + high)/2;
-          int bl = h - al;
-          int alm1 = (al == 0)?Integer.MIN_VALUE:a[al-1];
-          int alm2 = (al == n)?Integer.MAX_VALUE:a[al];
-          int blm1 = (bl == 0)?Integer.MIN_VALUE:b[bl-1];
-          int blm2 = (bl == m)?Integer.MAX_VALUE:b[bl];
-          if(alm1 <= blm2 && blm1 <= alm2) {
-              double ans = 0;
-              if((t & 1) == 1) {
-                  ans = Math.max(alm1, blm1)/1.0;
-              }
-              else {
-                  ans = (Math.max(alm1, blm1)+Math.min(alm2, blm2))/2.0;
-              }
-              return ans;
-          }
-          else if(alm1 > blm2) {
-              high = al - 1;
-          }
-          else {
-              low = al + 1;
-          }
-      }
-      return 0.0;
-  }
+  public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int [] nums = new int[n+m];
+        int j = 0;
+        for(int i = 0;i < n;i++){
+            nums[j++] = nums1[i];
+        }
+        for(int i = 0;i < m;i++){
+            nums[j++] = nums2[i];
+        }
+        Arrays.sort(nums);
+        if((n+m) % 2 != 0){
+            double median =(double) nums[(n+m)/2];
+            return Double.parseDouble(String.format("%.5f",median));
+        }else{
+            double median = (double)(nums[(n+m-1)/2]+nums[(n+m)/2])/2;
+            return Double.parseDouble(String.format("%.5f",median));
+        }
+    }
 }
-
-// TC--> O(log(min(n, m))
-// SC--> O(1)
