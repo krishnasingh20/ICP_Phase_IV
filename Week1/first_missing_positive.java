fd236af70480b88a21ada64ea45c1e58c16a659c
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-16 16:41:44 +0530

    Create First missing positive

---
+++ Week1/first_missing_positive.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+    System.out.println(firstMissingPositive(arr));
+  }
+  public static int firstMissingPositive(int[] nums) {
+      HashSet<Integer> set = new HashSet<>();
+      for(int num: nums) {
+          set.add(num);
+      }
+      int val = 1;
+      while(set.contains(val)) {
+          val++;
+      }
+      return val;
+  }
+}
