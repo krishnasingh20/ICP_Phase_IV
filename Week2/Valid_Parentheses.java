f68ea1be1755f8d535d7ff2112b98edff98ac7f1
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-19 20:38:18 +0530

    Create Valid Parentheses

---
+++ Week2/Valid_Parentheses.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Valid_Parentheses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(validParentheses(s));
+    }
+    public static boolean validParentheses(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                st.push(s.charAt(i));
+            }
+            else {
+                if(st.isEmpty() || !isValid(st.peek(), s.charAt(i))) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return true;
+    }
+    public static boolean isValid(char o, char c) {
+        return (o == '(' && c == ')') || (o == '{' && c == '}') || (o == '[' && c == ']');
+    }
+}
